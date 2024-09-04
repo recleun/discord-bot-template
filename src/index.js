33e2779d08891ccb479b05df356ed3fb96df3caf
recleun <98621222+recleun@users.noreply.github.com>
2024-09-05 01:14:16 +0300

    Add command interaction handling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import { Client, Colors, EmbedBuilder, Events, GatewayIntentBits } from "discord.js";
+import { Client, Colors, EmbedBuilder, Events, GatewayIntentBits, Collection } from "discord.js";
 import fs from "fs";
-import { loadGuilds, loadChannel } from "./utils/functions.js";
+import path from "path";
+import { loadGuilds, loadChannel, loadCommands } from "./utils/functions.js";
 
 const SECRETS = JSON.parse(fs.readFileSync("./secrets.json").toString());
 const CONFIG = JSON.parse(fs.readFileSync("./config.json").toString());
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+const commands = await loadCommands(path.join(import.meta.dirname, "./commands"));
+
+for (const c of commands) {
+	const command = (await import(c)).command;
+	console.log(`[LOG] Loading command ${command.data.name}`)
+	client.commands.set(command.data.name, command);
+}
 
 client.once(Events.ClientReady, async readyClient => {
 	await loadGuilds(CONFIG.guilds.dev.guild, "devGuild", client);
@@ -22,6 +32,27 @@ client.once(Events.ClientReady, async readyClient => {
 		});
 	}
 	console.log(`[LOG] Logged in as ${readyClient.user.username}`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+	if (!command) {
+		console.error(`[ERR] Command not found: ${interaction.commandName} -
+			${interaction.user.username} (${interaction.user.id})`);
+		return;
+	}
+	try {
+		await command.execute(interaction);
+	} catch (e) {
+		console.error(`[ERR] Command failed to execute: ${interaction.commandName} -
+			${interaction.user.username} (${interaction.user.id})`);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command.', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command.', ephemeral: true });
+		}
+	}
 });
 
 client.login(SECRETS.TOKEN);
