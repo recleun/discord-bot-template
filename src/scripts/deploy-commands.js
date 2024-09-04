5d5824d83b803be3bfd0acf47a6f9819e70c2147
recleun <98621222+recleun@users.noreply.github.com>
2024-09-05 00:44:47 +0300

    Create deploy scripts with npm scripts

---
+++ src/scripts/deploy-commands.js
@@ -0,0 +1,26 @@
+import { REST, Routes } from "discord.js";
+import fs from "fs";
+import path from "path";
+import { loadCommands } from "../utils/functions.js";
+
+const SECRETS = JSON.parse(fs.readFileSync("./secrets.json").toString());
+const commands = [];
+const commandFiles = await loadCommands(path.join(import.meta.dirname, "../commands"));
+
+for (const file of commandFiles) {
+	 commands.push((await import(file)).command.data.toJSON());
+}
+
+const rest = new REST().setToken(SECRETS.TOKEN);
+
+try {
+	console.log(`[LOG] Started globally deploying ${commands.length} slash commands.`);
+	const data = await rest.put(
+		Routes.applicationCommands(SECRETS.APPLICATION_ID),
+		{ body: commands },
+	);
+	console.log(`[LOG] Successfully deployed ${data.length} global slash commands.`);
+} catch (e) {
+	console.error(`[ERR] Failed to deploy global slash commands: ${e}`);
+}
+
---
+++ src/scripts/deploy-dev-commands.js
@@ -0,0 +1,27 @@
+import { REST, Routes } from "discord.js";
+import fs from "fs";
+import path from "path";
+import { loadCommands } from "../utils/functions.js";
+
+const SECRETS = JSON.parse(fs.readFileSync("./secrets.json").toString());
+const CONFIG = JSON.parse(fs.readFileSync("./config.json").toString());
+const commands = [];
+const commandFiles = await loadCommands(path.join(import.meta.dirname, "../commands"));
+
+for (const file of commandFiles) {
+	 commands.push((await import(file)).command.data.toJSON());
+}
+
+const rest = new REST().setToken(SECRETS.TOKEN);
+
+try {
+	console.log(`[LOG] Started globally deploying ${commands.length} slash commands.`);
+	const data = await rest.put(
+		Routes.applicationGuildCommands(SECRETS.APPLICATION_ID, CONFIG.guilds.dev.guild),
+		{ body: commands },
+	);
+	console.log(`[LOG] Successfully deployed ${data.length} global slash commands.`);
+} catch (e) {
+	console.error(`[ERR] Failed to deploy global slash commands: ${e}`);
+}
+
