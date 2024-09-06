2e09908266eddf850dccbdba47f0becbfa7d9e02
recleun <98621222+recleun@users.noreply.github.com>
2024-09-06 16:12:48 +0300

    Create logger.js

---
+++ src/utils/logger.js
@@ -0,0 +1,36 @@
+import { CommandInteraction } from "discord.js";
+import { parseCommandLog } from "./functions";
+
+export default {
+	/**
+	* @param {String} msg
+	*/
+	log(msg) {
+		console.log(`[LOG] ${msg}`);
+	},
+	/**
+	* @param {String} msg
+	*/
+	info(msg) {
+		console.log(`[INFO] ${msg}`);
+	},
+	/**
+	* @param {String} msg
+	*/
+	error(msg) {
+		console.error(`[ERR] ${msg}`);
+	},
+	/**
+	* @param {String} msg
+	*/
+	warn(msg) {
+		console.warn(`[WARN] ${msg}`);
+	},
+	/**
+	* @param {CommandInteraction} interaction
+	*/
+	command(interaction) {
+		console.log(`[CMD] ${parseCommandLog(interaction)}`);
+	},
+}
+
